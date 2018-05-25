4a80f24563a5aca854779257d5f49d2a98c21ae1
Maxim Ivaneychyk <maxim.ivaneychyk.cr@gmail.com>
2018-05-25 15:45:14 +0300

    server start conf

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let config = require('./config');
 let express = require('express');
 let server = require('./server');
+let cors = require('cors');
 let app = express();
+
+app.use(cors());
 
 app.listen(config.server.PORT, server.start.bind(server, app));
