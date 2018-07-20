5465f37d4461058e550bce400978acc83a6e445e
Maxim Ivaneychyk <maxim.ivaneychyk.cr@gmail.com>
2018-07-20 14:37:07 +0300

    -fix search users

--- server/middlewares/user/searchUsers.js
+++ server/middlewares/user/searchUsers.js
@@ -6,11 +6,17 @@ let errorMessages = require('../../errors/errorMessages');
 
 module.exports = function (req, res, next) {
     let query = `
-    SELECT  ${USERS}.id, ${USERS}.firstName, ${USERS}.lastName, ${PHOTOS}.url AS 'avatarUrl'
-    FROM ${USERS}
-    LEFT JOIN (SELECT photoId FROM ${AVATARS} WHERE ${AVATARS}.ownerId = ${USERS}.id ORDER BY createdAt DESC LIMIT 1) AS avatar ON avatar.ownerId = ${USERS}.id
-    LEFT JOIN ${PHOTOS} ON avatar.photoId = ${PHOTOS}.id
-    LIMIT ${LIMIT.USERS}`;
+    SELECT  Users.id, Users.firstName, Users.lastName, 
+    (
+      SELECT Photos.url 
+      FROM Avatars, Photos 
+      WHERE ownerId=Users.id AND Avatars.photoId=Photos.id 
+      ORDER BY Avatars.createdAt DESC 
+      LIMIT 1
+    ) AS 'avatarUrl'
+    FROM Users
+    LIMIT 10
+`;
 
     database.query(query).then(([rows]) => {
         res.ans.merge({data: rows});
