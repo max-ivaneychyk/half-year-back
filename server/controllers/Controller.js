c0c9f0c4b113b5c7d99a5c989a6b2a6dc64e4179
Maxim Ivaneychyk <maxim.ivaneychyk.cr@gmail.com>
2018-08-27 19:15:53 +0300

    Walls controller

--- server/controllers/Controller.js
+++ server/controllers/Controller.js
@@ -1,8 +1,21 @@
 const middlewares = require('../middlewares');
 
 class Controller {
+    
+    checkToken (req, res, next) {
+
+    }
+
     sendAnswer (req, res, next) {
         middlewares.sendAnswer(req, res, next)
+    }
+
+    addUserIdToParams(req, res, next) {
+
+    }
+
+    groupJoinData(req, res, next) {
+
     }
 }
 
